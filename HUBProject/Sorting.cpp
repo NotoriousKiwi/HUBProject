e3ffd43a38f8ebb644b95072a81071841215359a
Michael Clark <michael.bw.clark@gmail.com>
2018-09-01 23:09:30 +1000

    Array Extensions
    - Added in some extensions to the BubbleSorting functionality.
    - Begun BinarySearch functionality (feature currently incomplete.)

--- HUBProject/Sorting.cpp
+++ HUBProject/Sorting.cpp
@@ -38,6 +38,80 @@ void Sorting::BubbleSort(int bubbleArray[], int n)
 	}
 }
 
+void Sorting::PrintArray(int bubbleArray[], int n)
+{
+	for (int i = 0; i != n; ++i)
+	{
+		std::cout << bubbleArray[i] << ",\t";
+		if (i % 10 == 9) std::cout << std::endl;
+	}
+	std::cout << std::endl;
+	std::cout << std::endl;
+}
+
+void Sorting::BinarySetup()
+{
+	std::sort(sampleBinaryArray, sampleBinaryArray + 25);
+
+	while (true)
+	{
+		std::cout << std::endl << std::endl << "Enter a number to find in the sequence: -1 to end" << std::endl;
+		int searchKey = -1;
+		std::cin >> searchKey;
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		if (searchKey == -1)
+		{
+			break;
+		}
+	}
+}
+
+int Sorting::BinarySearch(int binaryArray[], int start_index, int end_index, int key)
+{
+	std::sort(binaryArray, binaryArray + 25);
+
+	while (true)
+	{
+		std::cout << std::endl << std::endl << "Enter a number to find in the sequence: -1 to end" << std::endl;
+		int searchKey = -1;
+		std::cin >> searchKey;
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		if (searchKey == -1)
+		{
+			break;
+		}
+	}
+
+	while (start_index <= end_index)
+	{
+		int pivot = (start_index + end_index) / 2;
+
+		if (binaryArray[pivot] == key)
+		{
+			return pivot;
+		}
+
+		if (key < binaryArray[pivot])
+			end_index = pivot - 1;
+		else
+			start_index = pivot + 1;
+	}
+
+	return -1;
+}
+
+void Sorting::DisplayArrays()
+{
+	// Displays the unsorted version of the array.
+	std::cout << "Unsorted:" << std::endl;
+	PrintArray(sampleBubbleArray, 50);
+
+	// Displays the sorted version of the array.
+	BubbleSort(sampleBubbleArray, 50);
+	std::cout << "Sorted:" << std::endl;
+	PrintArray(sampleBubbleArray, 50);
+}
+
 Sorting::~Sorting()
 {
 }
--- HUBProject/Sorting.h
+++ HUBProject/Sorting.h
@@ -2,6 +2,7 @@
 
 #include <array>
 #include <algorithm>
+#include <iostream>
 
 using std::swap;
 
@@ -13,13 +14,39 @@ protected:
 
 private:
 
+	int sampleBubbleArray[50] = 
+	{
+		14,65,63,1,54,
+		89,84,9,98,57,
+		71,18,21,84,69,
+		28,11,83,13,42,
+		64,58,78,82,13,
+		9,96,14,39,89,
+		40,32,51,85,48,
+		40,23,15,94,93,
+		35,81,1,9,43,
+		39,15,17,97,52
+	};
 
+	int sampleBinaryArray[25] = 
+	{
+		14,65,63,1,54,
+		89,84,9,98,57,
+		71,18,21,84,69,
+		28,11,83,13,42,
+		64,58,78,82,13
+	};
 
 public:
 	Sorting();
 
+	int BinarySearch(int binaryArray[], int start_index, int end_index, int key);
+
 	void Swap(int& a, int& b);
 	void BubbleSort(int bubbleArray[], int n);
+	void PrintArray(int bubbleArray[], int n);
+	void BinarySetup();
+	void DisplayArrays();
 
 	~Sorting();
 };
